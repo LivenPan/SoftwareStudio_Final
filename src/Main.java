ecdf5cc8bed61144e9b5cb7867b91f850d9404ee
SaberYhm <s103062124@m103.nthu.edu.tw>
2016-06-16 16:53:14 +0800

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import java.io.IOException;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class Main {
-	public static void main(String args[]) throws IOException{
+	public static void main(String[] args)throws IOException{
 		GameStage gs = new GameStage();
+		/*Mywindow w1 = new Mywindow();
+		w1.doSomeWork();*/
+		/*Thread s = new Thread(w1);
+		s.start();*/
 	}
+	
 }
